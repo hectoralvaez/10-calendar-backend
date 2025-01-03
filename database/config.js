7e05118fcd10b4fba6a685f3eb703c17c91962f6
Héctor <hectoralvaez@gmail.com>
2025-01-03 11:36:41 +0100

    Error a la hora de iniciar la BD

--- database/config.js
+++ database/config.js
@@ -4,9 +4,9 @@ const dbConnection = async() => {
     try {
         await mongoose.connect( process.env.DB_CNN);
         console.log('DB Online');
-
+        
     } catch (error) {
-        console.log(error);
+        console.error('Error al conectar a la base de datos:', error.message);
         throw new Error('Error a la hora de iniciar la BD');
     }
 }
