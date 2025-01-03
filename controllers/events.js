5647e8d6be6e5f987aedf07e27601d890a4debef
Héctor <hectoralvaez@gmail.com>
2025-01-03 08:05:32 +0100

    ⭐⭐⭐ 📅 D 📖 🌐 399. Eliminar Eventos (v1)

--- controllers/events.js
+++ controllers/events.js
@@ -87,12 +87,48 @@ const updateEvent = async(req, res = response) => {
 }
 
 // DELTE
-const deleteEvent = (req, res = response) => {
+const deleteEvent = async(req, res = response) => {
     
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    });
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await Event.findById( eventId );
+
+        if( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+                
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+
+        await Event.findByIdAndDelete(eventId);
+
+        res.json({
+            ok: true,
+            msg: 'deleteEvent',
+        });
+    }  
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
